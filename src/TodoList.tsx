b0379aadeef3d93fce694bc8a650a213f133437f
SOHORABHOSSAINTAREK <sh.tarek.9123@gmail.com>
2023-09-18 00:20:19 +0600

    complete

---
+++ src/TodoList.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+interface item {
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
+const TodoList: React.FC = () => {
+    const [todos, setTodos] = useState<item[]>([]);
+    const [input, setInput] = useState<string>("")
+
+    const handleToggle = (id: number) => {
+        setTodos(
+            todos.map((todo) => {
+                if (todo.id === id) {
+                    return { ...todo, completed: !todo.completed }
+                }
+                return todo;
+            })
+        )
+    };
+    const handleClick = () => {
+        const newTodo: item = { id: Date.now(), text: input, completed: false }
+        setTodos([...todos, newTodo]);
+    }
+    return (
+        <div className='main-container'>
+            <h1>Todo List</h1>
+            <ul>
+                {
+                    // todos.map(todo => <li key={todo.id} style={{ textDecoration: todo.completed ? "line-through" : "none" }} onClick={() => handleToggle(todo.id)}>{todo.text}</li>)
+                    todos.map((todo) => (
+                        <li key={todo.id} style={{ textDecoration: todo.completed ? "line-through" : "none", color: todo.completed ? "green" : "red" }}>
+                            {todo.text}
+                            <button className='complete-btn' onClick={() => handleToggle(todo.id)}>Completed</button>
+                        </li>
+                    ))
+
+                }
+            </ul>
+            <input type="text" placeholder='Enter Your Task'
+                onChange={(e) => setInput(e.currentTarget.value)} />
+            <button className='add-button' onClick={handleClick}>Add</button>
+        </div>
+    );
+};
+
+export default TodoList;
